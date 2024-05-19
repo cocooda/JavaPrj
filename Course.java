575c778035808de2bd733573ef172244b59e5b1f
cocooda <121653227+cocooda@users.noreply.github.com>
2024-05-19 19:24:29 +0700

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,19 +1,16 @@
 public class Course {
-    private String courseID;
     private String courseName;
     private int credits;
     private String courseDescription;
 
     // Constructor
     public Course (){
-        this.courseID = " ";
         this.courseName = " ";
         this.credits = 0;
         this.courseDescription = " ";
     }
 
-    public Course(String courseId, String courseName, int credits, String courseDescription) {
-        this.courseID = courseId;
+    public Course( String courseName, int credits, String courseDescription) {
         this.courseName = courseName;
         this.credits = credits;
         this.courseDescription = courseDescription;
@@ -24,9 +21,6 @@ public class Course {
     }
     
     // Getters
-    public String getCourseID() {
-        return courseID;
-    }
 
     public String getCourseName() {
         return courseName;
@@ -41,10 +35,6 @@ public class Course {
     }
 
     // Setters
-    public void setCourseId(String courseID) {
-        this.courseID = courseID;
-    }
-
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
